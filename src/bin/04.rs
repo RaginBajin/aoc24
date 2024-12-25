bebbf89a2e30ee39c9be304c71495a24422e90e7
Joe Bajin <josephbajin@gmail.com>
2024-12-25 21:17:15 +0000

    Day 4 - Part 2 Figured out this all on my own. YAY! Needed a little help with the way regex works and how to get data but that wasn't a big deal.

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -2,7 +2,7 @@ advent_of_code::solution!(4);
 use std::error::Error;
 use regex::Regex;
 
-fn extract_diagonals(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
+fn extract_diagonals_vertical(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let lines: Vec<&str> = input.lines().collect();
     let rows = lines.len();
     let cols = lines[0].len();
@@ -66,6 +66,30 @@ fn extract_diagonals(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(diagonals)
 }
 
+fn extract_diagonals(input: &str, row: usize, col: usize) -> Vec<String> {
+    let lines: Vec<&str> = input.lines().collect();
+    let rows = lines.len();
+    let cols = lines[0].len();
+
+    if rows <= (row + 1) || cols <= (col + 1) {
+        return Vec::new();
+    }
+
+    let mut diagonals = Vec::new();
+
+    // Top-left to bottom-right diagonals
+    let tl = format!("{}{}{}", lines[row - 1].chars().nth(col - 1).unwrap(),
+                lines[row].chars().nth(col).unwrap(),
+                lines[row + 1].chars().nth(col + 1).unwrap());
+    diagonals.push(tl);
+    // Top-right to bottom-left diagonals
+    let tr = format!("{}{}{}", lines[row - 1].chars().nth(col + 1).unwrap(),
+                lines[row].chars().nth(col).unwrap(),
+                lines[row + 1].chars().nth(col - 1).unwrap());
+    diagonals.push(tr);
+
+    diagonals
+}
 
 pub fn part_one(input: &str) -> Option<u64> {
     let word = "XMAS";
@@ -77,7 +101,7 @@ pub fn part_one(input: &str) -> Option<u64> {
 
     // Collect lines and diagonals into a unified data vector
     let mut data: Vec<&str> = input.lines().collect();
-    let diagonals = extract_diagonals(input).unwrap();
+    let diagonals = extract_diagonals_vertical(input).unwrap();
     let mut diagonals_refs: Vec<&str> = diagonals.iter().map(|s| s.as_str()).collect();
     data.append(&mut diagonals_refs);
 
@@ -99,8 +123,41 @@ pub fn part_one(input: &str) -> Option<u64> {
 
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let _ = input;
-    None
+    let word = "MAS";
+
+    // Reverse the word for the backward regex
+    let reverse_word = word.chars().rev().collect::<String>();
+    let foward_re = Regex::new(word).unwrap();
+    let backward_re = Regex::new(&reverse_word).unwrap();
+
+    // Collect lines and diagonals into a unified data vector
+    let data: Vec<&str> = input.lines().collect();
+    let rows = data.len();
+    let cols = data[0].len();
+
+   // println!("Data: {:?}", data);
+    let mut total = 0;
+
+    for r in 1..=(rows - 1){ // Start with Row 1 to find the 'A' and end 1 before
+        for c in 1..=(cols - 1) { // Start with Col 1 to find the 'A' and end 1 before
+            let mut sub_total = 0;
+
+            // Step 1: Search for A in each row of Data
+            if data[r].chars().nth(c).unwrap() == 'A' {
+                let check_data = extract_diagonals(input, r, c);
+                let diag_data: Vec<&str> = check_data.iter().map(|s| s.as_str()).collect();
+                for line in diag_data.iter() {
+                    sub_total += foward_re.find_iter(line).count() as u64;
+                    sub_total += backward_re.find_iter(line).count() as u64;
+                }
+                if sub_total == 2 {
+                    total += 1;
+                }
+            }
+        }
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -116,6 +173,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
+
