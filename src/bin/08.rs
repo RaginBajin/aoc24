e5bb632771af5e173d1d0de3de15156581fae94e
Joe Bajin <josephbajin@gmail.com>
2024-12-30 19:20:35 +0000

    `cargo fmt` Changes thats all

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,8 @@
 advent_of_code::solution!(8);
-use std::{collections::{HashMap, HashSet}, ops::{Add, AddAssign, Sub}};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::{Add, AddAssign, Sub},
+};
 
 #[derive(Clone, Copy, Default, Hash, Eq)]
 struct Point(i32, i32);
@@ -30,7 +33,6 @@ impl AddAssign for Point {
     fn add_assign(&mut self, other: Self) {
         *self = Self(self.0 + other.0, self.1 + other.1)
     }
-    
 }
 
 struct Map {
@@ -39,7 +41,7 @@ struct Map {
 }
 
 impl Map {
-    fn get(&self, Point(x,y ): Point) -> Option<u8> {
+    fn get(&self, Point(x, y): Point) -> Option<u8> {
         self.grid.get(x as usize)?.get(y as usize).copied()
     }
     fn get_pairs(&self) -> Vec<(Point, Point)> {
@@ -59,9 +61,9 @@ impl Map {
         let pairs = self.get_pairs();
 
         for (p1, p2) in pairs {
-            [p1 + (p1-p2), p2 + (p2-p1)]
+            [p1 + (p1 - p2), p2 + (p2 - p1)]
                 .into_iter()
-                .filter(move |&antinode| self.get(antinode).is_some() )
+                .filter(move |&antinode| self.get(antinode).is_some())
                 .for_each(|antinode| {
                     antinodes.insert(antinode);
                 })
@@ -74,11 +76,10 @@ impl Map {
         let pairs = self.get_pairs();
 
         for (p1, p2) in pairs {
-            [(p1, p1-p2), (p2, p2-p1)]
+            [(p1, p1 - p2), (p2, p2 - p1)]
                 .into_iter()
-                .for_each(| (mut point, offset) |   {
-                    while self.get(point).is_some()
-                    {
+                .for_each(|(mut point, offset)| {
+                    while self.get(point).is_some() {
                         antinodes.insert(point);
                         point += offset;
                     }
@@ -87,7 +88,6 @@ impl Map {
         antinodes
     }
 }
-
 
 impl From<&str> for Map {
     fn from(input: &str) -> Self {
@@ -99,18 +99,15 @@ impl From<&str> for Map {
                 row.iter().enumerate().filter_map(move |(y, &cell)| {
                     (cell != b'.').then_some((cell, Point(x as i32, y as i32)))
                 })
-            }).fold(HashMap::new(), |mut antennas, (sym, pos)| {
+            })
+            .fold(HashMap::new(), |mut antennas, (sym, pos)| {
                 antennas.entry(sym).or_insert_with(Vec::new).push(pos);
                 antennas
             });
 
-        Self {
-            grid,
-            antennas
-        }
+        Self { grid, antennas }
     }
 }
-
 
 pub fn part_one(input: &str) -> Option<u64> {
     let map = Map::from(input);
